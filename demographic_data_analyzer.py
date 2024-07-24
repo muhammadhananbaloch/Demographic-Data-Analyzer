48f4d9d43d3d14a9bca2510ae32c173700d0c5f9
Muhammad Hanan Baloch <muhammadhananbaloch@outlook.com>
2024-07-24 19:04:31 +0000

    Calculate average age of men and store in variable.

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -9,7 +9,7 @@ def calculate_demographic_data(print_data=True):
     race_count = df["race"].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df[df['sex'] == 'Male']['age'].mean()
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = None
@@ -67,3 +67,4 @@ def calculate_demographic_data(print_data=True):
         highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
+# df = pd.read_csv('adult.data.csv')
