fdbfdd0e026d7378d9a60885b8eb0a50f9f8e4be
Muhammad Hanan Baloch <muhammadhananbaloch@outlook.com>
2024-07-25 17:06:16 +0000

    Filter highest percentage country.

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -46,6 +46,7 @@ def calculate_demographic_data(print_data=True):
     highEarners = df[df['salary'] == '>50K']
     highEarnerPerCountry = highEarners['native-country'].value_counts()
     percentageHighEarnersPerCountry = highEarnerPerCountry * 100 / totalPeoplePerCountry
+    highest_earning_country = percentageHighEarnersPerCountry.idxmax()
     highest_earning_country = None
     highest_earning_country_percentage = None
 
